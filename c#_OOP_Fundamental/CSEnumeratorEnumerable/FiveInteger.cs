3cc6b341d9eb1cba663e86f258380cd0a6a919c2
Abanoub_Magdy <62855040+abanoubmagdy4@users.noreply.github.com>
2024-09-13 23:47:03 +0300

    Add files via upload

---
+++ c#_OOP_Fundamental/CSEnumeratorEnumerable/FiveInteger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSEnumeratorEnumerable
+{
+    public class FiveInteger
+    {
+        int[] _values; 
+        public FiveInteger(int n1 ,  int n2 , int n3 , int n4 , int n5) {
+            _values = new[] { n1, n2, n3, n4, n5 };
+    }
+    }
+}
---
+++ c#_OOP_Fundamental/CSEnumeratorEnumerable/Test.cs
@@ -0,0 +1,38 @@
+﻿namespace CSEnumeratorEnumerable
+{
+    public class Test
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is Test)) return false;
+
+            var emp = obj as Test;
+            return this.Id == emp.Id
+                  && this.Name == emp.Name
+                  && this.Address == emp.Address;
+        }
+
+        public Test(int id, string name, string address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+            hash = (hash * 7) +Id.GetHashCode();
+            return hash;
+        }
+        public static bool operator ==(Test lhs , Test rhs) => lhs.Equals(rhs);
+        public static bool operator !=(Test lhs, Test rhs) => !lhs.Equals(rhs);
+
+    }
+}
