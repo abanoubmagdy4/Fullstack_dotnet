62899d45bc998b4373464e5b97cd60ac49d8fa12
Abanoub_Magdy <62855040+abanoubmagdy4@users.noreply.github.com>
2024-09-13 23:27:14 +0300

    Add files via upload

---
+++ c#_OOP_Fundamental/CAPackageing/FBComment.cs
@@ -0,0 +1,20 @@
+﻿using Humanizer;
+namespace CAFileStream
+{
+    class FBComment
+    { 
+       public string Owner {get;set;}
+        public string Comment {get; set;}
+        public DateTime CreatedAt {get;set;}
+
+        public override string ToString()
+        {
+            return $"{Owner} says : \n" +
+                   $"\t\t\"{Comment}\"" +
+                   $"\n\t\t\t\t {CreatedAt.Humanize()}";
+        }
+
+
+    }
+
+}// End Of Namespace
