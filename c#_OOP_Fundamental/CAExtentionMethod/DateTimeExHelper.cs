f7ca579452840ada68987a1e31f2f36c3b3535f9
Abanoub_Magdy <62855040+abanoubmagdy4@users.noreply.github.com>
2024-09-13 23:46:00 +0300

    Add files via upload

---
+++ c#_OOP_Fundamental/CAExtentionMethod/DateTimeExHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAExtentionMethod
+{
+    public static class DateTimeExHelper
+    {
+        public static bool IsWeekEnd(this DateTime value)
+        {
+  // this represent the method extention from DateTime its take us to can reusable method related DateTime Class
+            return value.DayOfWeek == DayOfWeek.Saturday || value.DayOfWeek == DayOfWeek.Sunday ; 
+        
+        }
+        public static bool ISWeekDay(this DateTime value)
+
+        {
+            return !IsWeekEnd(value);
+          
+        }
+        
+    }
+}
