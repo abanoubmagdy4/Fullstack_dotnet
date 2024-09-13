e8a1b10e9fad7ef18d7a4fab60612d0bb8b2a9fb
Abanoub_Magdy <62855040+abanoubmagdy4@users.noreply.github.com>
2024-09-13 22:54:16 +0300

    Add files via upload

---
+++ c#_OOP_Fundamental/CAPizzaApp/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPizza
+{
+    internal class Order
+    {
+        protected int orderID { get; set; }
+        protected string orderName { get; set; }
+        
+
+        Order(int orderid, string oredername) {
+
+            this.orderID = orderid;
+            this.orderName = oredername;
+        }
+
+        public string PlaceOrder(string place) {
+            Console.WriteLine("Inter Your Place");
+            place = place.ToLower();    
+            return place;
+        }
+        public void RreserveOrder() { 
+            var reserve = 
+        
+        }
+
+    }
+}
